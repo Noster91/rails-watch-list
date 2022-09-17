e020d13d398d5aeffb2ae2220b147ee375fbdde8
julian chadwick <noster91@gmail.com>
2022-09-17 15:40:15 -0300

    Controller y Vistas de Bookmark Done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [:create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = @list.id
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/show', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
